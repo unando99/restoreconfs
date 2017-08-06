eec2f3a26f5f14ef1dc0ac36576ecadfd3eb1bcd
unando99 <unando99@gmail.com>
2017-08-06 00:15:21 -0700

    added software

--- bashscript.sh
+++ bashscript.sh
@@ -2,4 +2,8 @@
 
 echo "Nando's config restore script."
 
-sudo apt update && sudo apt upgrade -y && echo "completed"
+sudo apt update && sudo apt upgrade -y
+
+echo "Update Complete. Proceding with software install"
+
+sudo apt install -y compton i3 i3lock feh volumeicon-alsa
