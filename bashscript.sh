3355635c29b88bac37140a39a48ee64fe958a7b5
unando99 <unando99@gmail.com>
2017-08-06 00:58:32 -0700

    added copy configs

--- bashscript.sh
+++ bashscript.sh
@@ -7,3 +7,11 @@ sudo apt update && sudo apt upgrade -y
 echo "Update Complete. Proceding with software install"
 
 sudo apt install -y compton i3 i3lock feh volumeicon-alsa
+
+echo "Completed software install. Copying config files"
+
+cp compton.conf ~/.config/
+cp i3/config ~/.config/i3/
+cp i3status/config ~/.config/i3status/
+
+echo "Config files done. Your system should now be restored."
