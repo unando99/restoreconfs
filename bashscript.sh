4480f0833f50ac44a30b4c771244ffd7f49fc48e
unando99 <unando99@gmail.com>
2017-08-06 01:13:22 -0700

    added pauses

--- bashscript.sh
+++ bashscript.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 echo "Nando's config restore script."
+sleep 5
 
 sudo apt update && sudo apt upgrade -y
 
 echo "Update Complete. Proceding with software install"
+sleep 5
 
 sudo apt install -y compton i3 i3lock feh volumeicon-alsa
 
 echo "Completed software install. Copying config files"
+sleep 5
 
 cp compton.conf ~/.config/
 cp i3/config ~/.config/i3/
